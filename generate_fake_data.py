52a95af439af40aa4331d0b67b478de71853c396
Josh Dev <imjbmkz@gmail.com>
2024-09-01 00:00:27 +0800

    Initialize generate fake data app

---
+++ generate_fake_data.py
@@ -0,0 +1,50 @@
+import json
+from sys import argv
+from pathlib import Path
+from shutil import rmtree
+from faker import Faker
+from tqdm import tqdm
+
+if __name__=="__main__":
+
+    # Control the number of records
+    if len(argv)>=2:
+        records = int(argv[1])
+    else:
+        records = 500_000
+
+    # Delete existing folder with dummy files
+    folder = Path("files")
+    if folder.is_dir():
+        rmtree(folder)
+
+    # Recreate the folder 
+    folder.mkdir(exist_ok=True)
+
+    fake = Faker()
+
+    # Generate 500k fake records
+    for i in tqdm(range(1,records + 1)):
+
+        # Dictionary of fake data
+        fake_data = {
+            # Identity
+            "id": i,
+            "full_name": fake.name(),
+            "ssn": fake.ssn(),
+            "dob": fake.date_of_birth().strftime("%Y-%m-%d"),
+            
+            # Contact details
+            "email": fake.email(),
+            "phone": fake.phone_number(),
+            
+            # Location
+            "address": fake.address(),
+            "city": fake.city(),
+            "state": fake.state(),
+            "zipcode": fake.zipcode(),
+            "country": fake.country(),
+        }
+
+        with open(folder / f"{i}.json", "w") as fp:
+            json.dump(fake_data, fp)
