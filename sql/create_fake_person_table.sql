fda6882a3f83a75c2685fef6d3816177d9d7eb05
Josh Dev <imjbmkz@gmail.com>
2024-09-03 00:05:43 +0800

    Added docker-compose for postgresdb and pgadmin

---
+++ sql/create_fake_person_table.sql
@@ -0,0 +1,16 @@
+drop table fake_person;
+create table fake_person (
+	id bigint,
+	full_name varchar(256),
+	is_male boolean,
+	dob date,
+	ssn varchar(50),
+	email varchar(256),
+	phone varchar(50),
+	address varchar(256),
+	city varchar(50),
+	state varchar(50),
+	zipcode varchar(50),
+	country varchar(50)
+)
+
